768d9b04996dca3b241808072ae6619e991abebb
Ilyas Mercan <ilyas.mercan@skynet.be>
2020-03-22 10:03:39 +0100

    refactored images, added credits

--- src/app/models/event.model.ts
+++ src/app/models/event.model.ts
@@ -1,16 +1,18 @@
+import {Picture} from './picture.model';
+
 export class Event {
   name: string;
   organizer: string;
   dates: Date[];
-  urlsOfPictures: string[];
+  picture: Picture;
   receivedHours: number;
   report: string;
 
-  constructor(name: string, organizer: string, dates: Date[], urlsOfPictures: string[], receivedHours: number, report: string) {
+  constructor(name: string, organizer: string, dates: Date[], picture: Picture, receivedHours: number, report: string) {
     this.name = name;
     this.organizer = organizer;
     this.dates = dates;
-    this.urlsOfPictures = urlsOfPictures;
+    this.picture = picture;
     this.receivedHours = receivedHours;
     this.report = report;
   }
---
+++ src/app/models/picture.model.ts
@@ -0,0 +1,9 @@
+export class Picture {
+  url: string;
+  credit: string;
+
+  constructor(url: string, credit: string) {
+    this.url = url;
+    this.credit = credit;
+  }
+}
